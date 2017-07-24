979e0d1070f3509a26b44ed91b3108f144de239c
Mustafa Turan <mustafaturan.net@gmail.com>
2017-07-24 01:48:00 +0100

    Bump to version 0.2.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule EventBus.Mixfile do
 
   def project do
     [app: :event_bus,
-     version: "0.2.0",
+     version: "0.2.1",
      elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
