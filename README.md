51d0b6e1b533bcdc5cd4da44d7badc866f2fc868
Mustafa Turan <mustafaturan.net@gmail.com>
2017-08-06 04:17:32 +0100

    Bump to version 0.4.1

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ The package can be installed by adding `event_bus` to your list of dependencies 
 
 ```elixir
 def deps do
-  [{:event_bus, "~> 0.4.0"}]
+  [{:event_bus, "~> 0.4.1"}]
 end
 ```
 
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule EventBus.Mixfile do
 
   def project do
     [app: :event_bus,
-     version: "0.4.0",
+     version: "0.4.1",
      elixir: "~> 1.5",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
