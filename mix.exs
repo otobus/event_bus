90d515d32ee81767464efb7ffcbe4423c2cb0450
Mustafa Turan <mustafaturan.net@gmail.com>
2018-05-28 00:31:33 -0700

    Bump version to 1.3.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EventBus.Mixfile do
   def project do
     [
       app: :event_bus,
-      version: "1.3.1",
+      version: "1.3.2",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
