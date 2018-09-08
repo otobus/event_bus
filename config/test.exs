062639e161625c9b3e7c94b14bdb4e8aef2405eb
Mustafa Turan <mustafaturan.net@gmail.com>
2018-09-07 18:39:50 -0700

    Update id generator module in test configuration

--- config/test.exs
+++ config/test.exs
@@ -4,4 +4,4 @@ config :event_bus,
   topics: [:metrics_received, :metrics_summed],
   ttl: 30_000_000,
   time_unit: :microsecond,
-  id_generator: EventBus.Util.String
+  id_generator: EventBus.Util.Base62
